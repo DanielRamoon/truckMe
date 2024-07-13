1ec278ad1fd0f0c26056bb048c9fa453eb5a86d0
Daniel Ramon <danielramoon@gmail.com>
2024-07-13 16:56:53 -0300

    component-banner

---
+++ truckme-vue/src/components/BannerComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="banner">
+    <img class="banner-image" :src="bannerImage" alt="Banner Image" />
+    <h1 class="banner-title">Seja bem-vindo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BannerComponent",
+  data() {
+    return {
+      bannerImage: require("@/assets/images/banner.jpeg"),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.banner {
+  position: relative;
+  width: 100%;
+  max-width: 100%;
+  overflow: hidden;
+  text-align: center;
+}
+
+.banner-image {
+  width: 100%;
+  height: auto;
+  display: block;
+}
+
+.banner-title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: black;
+  font-size: 4rem;
+  opacity: 0;
+  animation: fadeInUp 1.5s ease forwards;
+}
+
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    transform: translate(-50%, -60%);
+  }
+  to {
+    opacity: 1;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
--- truckme-vue/src/views/HomePage.vue
+++ truckme-vue/src/views/HomePage.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="home-page">
     <HeaderComponent />
+    <BannerComponent />
+    <!-- Outro conteúdo da página Home -->
   </div>
 </template>
 
 <script>
 import HeaderComponent from "@/components/HeaderComponent.vue";
+import BannerComponent from "@/components/BannerComponent.vue"; // Importe o BannerComponent
 
 export default {
   name: "HomePage",
   components: {
     HeaderComponent,
+    BannerComponent, // Adicione o BannerComponent aos componentes importados
   },
 };
 </script>
+
+<style scoped>
+/* Estilos específicos da página Home */
+.home-page {
+  /* Estilos da página Home */
+}
+</style>
